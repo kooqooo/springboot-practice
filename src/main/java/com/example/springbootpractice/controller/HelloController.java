ff4ff2a72f4d0cb5fb0ebad0a57cdf27fe4f5266
kooqooo <koogmlcks@gmail.com>
2025-05-10 15:02:44 +0900

    refactor: hello 서비스대신 기본값 파라미터 사용

--- src/main/java/com/example/springbootpractice/controller/HelloController.java
+++ src/main/java/com/example/springbootpractice/controller/HelloController.java
@@ -5,20 +5,12 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.example.springbootpractice.service.HelloService;
-
 @RestController
 @RequestMapping("/hello")
 public class HelloController {
 
-    private final HelloService helloService;
-    
-    public HelloController(HelloService helloService){
-        this.helloService = helloService;
-    }
-
     @GetMapping
-    public String sayHello(@RequestParam(value = "name", required = false) String name) {
-        return helloService.getHelloMessage(name);
+    public String sayHello(@RequestParam(value = "name", defaultValue="World!!!!") String name) {
+        return "Hello, " + name + "!";
     }
 }
